39cf2d8bcebb675ce9e4ba18fd1c741306699b68
Moukaskon <moukaskon04@gmail.com>
2025-05-19 17:31:54 +0300

    ref

--- src/ReportGenerator.java
+++ src/ReportGenerator.java
@@ -1,10 +1,16 @@
 public class ReportGenerator {
     public void generateReport() {
-        // Header
+        printHeader();
+        printData();
+        printFooter();
+    }
+
+    private void printHeader() {
         System.out.println("===== Monthly Report =====");
         System.out.println("Generated on: " + java.time.LocalDate.now());
+    }
 
-        // Data section
+    private void printData() {
         int totalSales = 5000;
         int totalCustomers = 123;
         double avgSale = totalSales / (double) totalCustomers;
@@ -12,8 +18,10 @@ public class ReportGenerator {
         System.out.println("Total Sales: $" + totalSales);
         System.out.println("Total Customers: " + totalCustomers);
         System.out.println("Average Sale per Customer: $" + avgSale);
+    }
 
-        // Footer
+    private void printFooter() {
         System.out.println("==========================");
     }
 }
+
